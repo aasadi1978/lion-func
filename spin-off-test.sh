115561ce2b7ddbf875d8ca63c7fac580db8929a8
Alireza Asadi <a.r.asadi@gmail.com>
2025-11-03 11:02:05 +0100

    Adding missing credential identifiers

--- spin-off-test.sh
+++ spin-off-test.sh
@@ -13,26 +13,26 @@ echo "Response body:"
 echo "$BODY"
 echo "HTTP Status: $HTTP_STATUS"
 
-echo "Spinning off a new container... DEFAULT_HOST_KEY"
+# echo "Spinning off a new container... DEFAULT_HOST_KEY"
 
-RESPONSE=$(curl -s -w "\nHTTP_STATUS:%{http_code}\n" -X POST "$DEFAULT_HOST_KEY&app_name=lion-task-1234&task_type=optimize")
+# RESPONSE=$(curl -s -w "\nHTTP_STATUS:%{http_code}\n" -X POST "$DEFAULT_HOST_KEY&app_name=lion-task-1234&task_type=optimize")
 
-# Extract HTTP status
-HTTP_STATUS=$(echo "$RESPONSE" | grep HTTP_STATUS | cut -d':' -f2)
-BODY=$(echo "$RESPONSE" | sed '/HTTP_STATUS/d')
+# # Extract HTTP status
+# HTTP_STATUS=$(echo "$RESPONSE" | grep HTTP_STATUS | cut -d':' -f2)
+# BODY=$(echo "$RESPONSE" | sed '/HTTP_STATUS/d')
 
-echo "Response body:"
-echo "$BODY"
-echo "HTTP Status: $HTTP_STATUS"s
+# echo "Response body:"
+# echo "$BODY"
+# echo "HTTP Status: $HTTP_STATUS"s
 
-echo "Spinning off a new container... MASTER_HOSTKEY"
+# echo "Spinning off a new container... MASTER_HOSTKEY"
 
-RESPONSE=$(curl -s -w "\nHTTP_STATUS:%{http_code}\n" -X POST "$MASTER_HOST_KEY&app_name=lion-task-1234&task_type=optimize")
+# RESPONSE=$(curl -s -w "\nHTTP_STATUS:%{http_code}\n" -X POST "$MASTER_HOST_KEY&app_name=lion-task-1234&task_type=optimize")
 
-# Extract HTTP status
-HTTP_STATUS=$(echo "$RESPONSE" | grep HTTP_STATUS | cut -d':' -f2)
-BODY=$(echo "$RESPONSE" | sed '/HTTP_STATUS/d')
+# # Extract HTTP status
+# HTTP_STATUS=$(echo "$RESPONSE" | grep HTTP_STATUS | cut -d':' -f2)
+# BODY=$(echo "$RESPONSE" | sed '/HTTP_STATUS/d')
 
-echo "Response body:"
-echo "$BODY"
-echo "HTTP Status: $HTTP_STATUS"
+# echo "Response body:"
+# echo "$BODY"
+# echo "HTTP Status: $HTTP_STATUS"
