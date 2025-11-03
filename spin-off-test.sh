7a78f93402605f3c4d3187bd451f7d1070960ffc
Alireza Asadi <a.r.asadi@gmail.com>
2025-11-03 10:00:48 +0100

    Modified yml file

--- spin-off-test.sh
+++ spin-off-test.sh
@@ -3,7 +3,31 @@
 source ./secrets.sh
 echo "Spinning off a new container..."
 
-RESPONSE=$(curl -s -w "\nHTTP_STATUS:%{http_code}\n" -X POST "https://lion-func-bqhycbhdfna4fder.westeurope-01.azurewebsites.net/api/createApp?app_name=lion-task-1234&task_type=optimize&code=$FUNC_KEY")
+RESPONSE=$(curl -s -w "\nHTTP_STATUS:%{http_code}\n" -X POST "$FUNC_KEY&app_name=lion-task-1234&task_type=optimize")
+
+# Extract HTTP status
+HTTP_STATUS=$(echo "$RESPONSE" | grep HTTP_STATUS | cut -d':' -f2)
+BODY=$(echo "$RESPONSE" | sed '/HTTP_STATUS/d')
+
+echo "Response body:"
+echo "$BODY"
+echo "HTTP Status: $HTTP_STATUS"
+
+echo "Spinning off a new container... DEFAULT_HOST_KEY"
+
+RESPONSE=$(curl -s -w "\nHTTP_STATUS:%{http_code}\n" -X POST "$DEFAULT_HOST_KEY&app_name=lion-task-1234&task_type=optimize")
+
+# Extract HTTP status
+HTTP_STATUS=$(echo "$RESPONSE" | grep HTTP_STATUS | cut -d':' -f2)
+BODY=$(echo "$RESPONSE" | sed '/HTTP_STATUS/d')
+
+echo "Response body:"
+echo "$BODY"
+echo "HTTP Status: $HTTP_STATUS"s
+
+echo "Spinning off a new container... MASTER_HOSTKEY"
+
+RESPONSE=$(curl -s -w "\nHTTP_STATUS:%{http_code}\n" -X POST "$MASTER_HOST_KEY&app_name=lion-task-1234&task_type=optimize")
 
 # Extract HTTP status
 HTTP_STATUS=$(echo "$RESPONSE" | grep HTTP_STATUS | cut -d':' -f2)
